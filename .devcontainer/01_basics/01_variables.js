7cb909bde0da4098f693180f0c7f59be8c7d07f0
Yuvraj Singh <1308yuvraj@gmail.com>
2025-07-12 17:27:04 +0530

    Variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 12345
+
+let accountEmail = "yuvraj@gmail.com"
+var accountPassword = "@123"
+accountCity = "Dhanbad"
+
+// accountId = 9876 --- not allowed
+
+accountEmail = "yuv@google.com"
+accountPassword = "0009"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+  Do not use var for declearing variables
+*/
