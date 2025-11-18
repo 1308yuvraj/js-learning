22a045795db03818d2ba40a460c9ac131842df3a
Yuvraj Singh <1308yuvraj@gmail.com>
2025-11-18 09:47:27 +0530

    new

---
+++ .devcontainer/04_control_flow.js/one.js
@@ -0,0 +1,41 @@
+// if
+
+// const isLoggedIn = true
+
+// if( 2 != 3){
+//     console.log("executed");
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const tempreature = 18
+
+// if(tempreature === 18){
+//     console.log("Temperature is less than 20");    
+// }else{
+//     console.log("Temperature is greater than 20");    
+// }
+
+// console.log("executed");
+
+// const score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);    
+// }
+
+// const balance = 1000
+
+// if(balance > 500) console.log("test"), console.log('test2');
+
+// if(balance < 500){
+//     console.log('less than 500');
+// }
+// else if(balance < 750){
+//         console.log('less than 750');
+// }
+// else if(balance < 1200){
+    //         console.log('less than 1200');
+// }
+
